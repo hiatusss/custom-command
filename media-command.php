f86f80f88de3d1b125e59dbd14fda684302b9b71
hiatusss <31628730+hiatusss@users.noreply.github.com>
2018-01-18 19:04:54 -0500

    Update media-command.php

--- media-command.php
+++ media-command.php
@@ -9,11 +9,4 @@ if ( file_exists( $autoload ) ) {
 	require_once $autoload;
 }
 
-WP_CLI::add_command( 'media', 'Media_Command', array(
-	'before_invoke' => function () {
-		if ( !wp_image_editor_supports() ) {
-			WP_CLI::error( 'No support for generating images found. ' .
-				'Please install the Imagick or GD PHP extensions.' );
-		}
-	}
-) );
+WP_CLI::add_command( 'custom', 'Custom_Command');
